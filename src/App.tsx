5dbc31b666eeb02ddce387b406ec606b941b1206
Anuraag Gupta <anuraag@invideo.io>
2021-04-15 20:00:31 +0530

    feat: elevator issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Elevator from './elevator/elevator'
 import { About } from './pages/About'
-import { Home } from './pages/Home'
 
 const App: React.FC = () => {
-  return (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/about" component={About} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/" component={Elevator} exact />
+                    <Route path="/about" component={About} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App
